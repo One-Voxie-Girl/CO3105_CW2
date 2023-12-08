831140e08e511b07a70c3c5ab8c049691c669a48
tem <onevoxiegirl@gmail.com>
2023-12-08 15:43:50 +0000

    Update to use relative path for filename

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include "Classes/Map/Map.h"
 
 int main(int argc, char* argv[]) {
-    json j = read_map_data("C:\\Users\\onevo\\CLionProjects\\CPP\\CO3105_CW2\\maps\\map1.json");
+    json j = read_map_data("..\\maps\\map1.json");
     Map map = init_map(j);
     cout << map.print() << endl;
     return 0;

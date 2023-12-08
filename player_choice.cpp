2a7dd87c3f0e5b6eec3bc4f46208ec42928b2da6
Rathguard <lee.milligan@hotmail.co.uk>
2023-12-08 17:13:47 +0000

    Player input is now properly split into the command and parameter ,(Works with multi-word parameter)

--- player_choice.cpp
+++ player_choice.cpp
@@ -1,15 +1,21 @@
 #include "player_choice.h"
 
 Map player_choice(Map map) {
-    vector<string> playerInput;
-    string playerInputToken;
-    string playerInputString;
+    string playerInput;
+    string input1;
+    string input2;
 
     cout << "What do you want to do?" << endl;
-    getline(cin, playerInputString);
-    stringstream playerInputStream(playerInputString);
-    while (getline(playerInputStream, playerInputToken, ' ')) {
-        playerInput.push_back(playerInputToken);
-    } // Splits the input string into individual words
+    getline(cin, playerInput);
+    transform(playerInput.begin(), playerInput.end(), playerInput.begin(), ::tolower);
+    istringstream inputStream(playerInput);
+    inputStream >> input1 >> input2;
+    while(!inputStream.eof()) {
+        input2 += " ";
+        string tempInput;
+        inputStream >> tempInput;
+        input2 += tempInput;
+    } // Splits the input string into the command and the parameter
+    cout << input1 << endl << input2;
     return map;
 }

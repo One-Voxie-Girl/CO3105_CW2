9c870efd55771bc99556ad258c8924511b36581e
Rathguard <lee.milligan@hotmail.co.uk>
2023-12-13 15:29:54 +0000

    Player can now select map file from folder

--- main.cpp
+++ main.cpp
@@ -1,13 +1,26 @@
 #include <iostream>
+#include <filesystem>
 #include "map_parser.h"
 #include "Classes/Map/Map.h"
 #include "player_choice.h"
 
 int main(int argc, char* argv[]) {
-    json j = read_map_data("..\\maps\\map1.json");
+    string path = "..\\maps\\";
+    string mapSelection;
+    cout << "LIST OF AVAILABLE MAPS:" << endl;
+    for (const auto & entry : filesystem::directory_iterator(path))
+        cout << entry.path().stem().string() << endl;
+    cout << "Which map would you like to play? " << endl;
+    cin >> mapSelection;
+
+    json j = read_map_data("..\\maps\\" + mapSelection + ".json");
     Map map = init_map(j);
-//    cout << map.print() << endl;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    for(int i = 0; i < 100; i++) {
+        cout << endl;
+    }
     cout << map.rooms[map.get_room_id(map.player.get_room())].print() << endl;
+
     while (true){
         player_choice(map);
         if (map.objective.check_obj(map.player)) {
